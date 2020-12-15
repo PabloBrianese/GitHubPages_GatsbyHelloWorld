8c092652098650e7da16482237872b1201a61124
PabloBrianese <pablobrianese@gmail.com>
2020-12-15 17:43:49 -0300

    Gatsby.js installation process

--- command_line_process.sh
+++ command_line_process.sh
@@ -4,5 +4,31 @@
 sudo apt update
 sudo apt upgrade
 
-#
-sudo apt-get install curl
+# We need curl to install Node Version Manager
+sudo apt install curl
+
+# Download nvm installation bash script and execute it
+stdout='-'
+curl --output $stdout https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
+
+nvm --version  # Confirm this has worked 
+
+# Install Node.js using the latest long term support (lts) version
+nvm install --lts
+nvm use --lts
+
+npm --version  # Confirm this has worked
+node --version
+
+# Install Git
+sudo apt install git
+
+git --version  # Confirm this has worked
+
+git config --global user.name "YourUserName"
+git config --global user.email "YourEmailAdress"
+
+# Install Gatsby CLI
+npm install --global gatsby-cli  # Ignore warnings??
+
+# Create a Gatsby site
